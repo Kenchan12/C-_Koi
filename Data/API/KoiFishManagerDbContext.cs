8a74979cbff7e3f937b885e0feb03e880279a9d6
Kenchan12 <toanlop10b2@gmail.com>
2024-11-20 10:03:54 +0700

    Add files via upload

---
+++ Data/API/KoiFishManagerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using KoiFishManager.Api.Entities;
+
+namespace KoiFishManager.Api.Data
+{
+    public class KoiFishManagerDbContext : IdentityDbContext<User, Role, Guid>
+    {
+        public KoiFishManagerDbContext(DbContextOptions<KoiFishManagerDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Pond> Ponds { get; set; }
+        public DbSet<KoiFish> KoiFishes { get; set; }
+        public DbSet<PondStatus> PondStatuses { get; set; }
+        public DbSet<FeedingSchedule> FeedingSchedules { get; set; }
+
+    }
+}
