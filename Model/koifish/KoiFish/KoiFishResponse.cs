9ae8175db4e9d9a001836247b240e99218ed31fc
Kenchan12 <toanlop10b2@gmail.com>
2024-11-22 00:21:14 +0700

    Add files via upload

---
+++ Model/koifish/KoiFish/KoiFishResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KoiFishManager.Models.KoiFish
+{
+    public class KoiFishResponse
+    {
+        public int Id { get; set; }                 // ID cá Koi
+        public string Name { get; set; } = string.Empty;           // Tên cá Koi
+        public string Color { get; set; } = string.Empty;         // Màu sắc cá Koi
+        public double Size { get; set; }          // Kích thước cá Koi
+        public string Origin { get; set; } = string.Empty;         // Xuất xứ cá Koi
+        public string HealthStatus { get; set; } = string.Empty;     // Tình trạng sức khỏe
+        public DateTime CreatedAt { get; set; } 
+        public int PondId { get; set; }
+    }
+}
---
+++ Model/koifish/KoiFish/PondDto.cs
@@ -0,0 +1,21 @@
+﻿using KoiFishManager.Models.Pond;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFishManager.Models.KoiFish
+{
+    public class PondDto
+    {
+
+        public int Id { get; set; }                     // ID của hồ cá
+        public string Name { get; set; }                // Tên hồ cá
+        public double Size { get; set; }                // Diện tích hồ cá
+        public double Depth { get; set; }               // Độ sâu của hồ cá
+        public string WaterQuality { get; set; }        // Chất lượng nước        
+        public DateTime CreatedAt { get; set; }         // Thời gian tạo
+        public DateTime UpdatedAt { get; set; }         // Thời gian cập nhật
+    }
+}
